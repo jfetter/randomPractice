f4472cc2fff2b8ed90be7001a2098e178551c98c
jfetter <jillian.fetter@gmail.com>
2015-08-15 18:00:44 -0700

    almost got item design worked out, but spacing is still off and js is not working

---
+++ localJS.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+$(function(){
+    var $select = $(".1-100");
+    for (i=1;i<=100;i++){
+        $select.append($('<option></option>').val(i).html(i))
+    }
+});​
+});
+
+function merch (){
+	this.price = getElementByClassName // price
+	this.quantity = getElementByClassName // quantity
+}
+
+var product = function (quantity, price){
+ quantity * price // get the price from the defined field of text
+ 
+
+};
